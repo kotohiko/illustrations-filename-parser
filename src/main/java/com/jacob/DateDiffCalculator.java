2ac4a0a41ca88ee76620760d2d19393221ae852c
yuzuki1231 <tachibana.kotohiko@gmail.com>
2023-01-09 21:46:34 +0800

    first

---
+++ src/main/java/com/jacob/DateDiffCalculator.java
@@ -0,0 +1,15 @@
+package com.jacob;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * 计算两个日期的天数差
+ */
+public class DateDiffCalculator {
+    public static int dateDiffCalculator(String fromDate, String toDate) {
+        LocalDate from = LocalDate.parse(fromDate);
+        LocalDate to = LocalDate.parse(toDate);
+        return (int) ChronoUnit.DAYS.between(from, to);
+    }
+}
---
+++ src/test/java/com/jacob/DateDiffCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.jacob;
+
+import java.util.Scanner;
+
+public class DateDiffCalculatorTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入起始日期（yyyy-mm-dd）：");
+        String fromDate = input.next();
+        System.out.print("请输入截止日期（yyyy-mm-dd）：");
+        String toDate = input.next();
+        System.out.println(DateDiffCalculator.dateDiffCalculator(fromDate,toDate));
+    }
+}
