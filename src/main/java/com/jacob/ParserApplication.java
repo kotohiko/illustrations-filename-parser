3bc3ce86ac7de7c270f33581f52cdcb2a4b6a23f
JacobSuen <tachibana.kotohiko@gmail.com>
2023-12-13 12:49:20 +0800

    支持解析以后直接通过浏览器来打开

--- src/main/java/com/jacob/ParserApplication.java
+++ src/main/java/com/jacob/ParserApplication.java
@@ -3,6 +3,7 @@ package com.jacob;
 import com.jacob.parse.ParsingFileNameToUrlEntrance;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * Startup class of the project.
@@ -11,7 +12,7 @@ import java.io.IOException;
  * @since Oct 25, 2023
  */
 public final class ParserApplication {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         ParsingFileNameToUrlEntrance.interact();
     }
 }
--- src/main/java/com/jacob/parse/ParsingFileNameToUrlEntrance.java
+++ src/main/java/com/jacob/parse/ParsingFileNameToUrlEntrance.java
@@ -1,8 +1,11 @@
 package com.jacob.parse;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Entrance of parsing filename to URL.
@@ -43,9 +46,10 @@ public final class ParsingFileNameToUrlEntrance {
 
     /**
      * Interaction logic.
+     *
      * @throws IOException I/O exception
      */
-    public static void interact() throws IOException {
+    public static void interact() throws IOException, URISyntaxException {
         System.out.println(ENTRANCE_MESSAGE);
         enterAccessCode();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -63,6 +67,12 @@ public final class ParsingFileNameToUrlEntrance {
                 String filename = line;
                 String out = CommonEnter.parseFileName(sideCodeValidation, filename);
                 System.out.println(out);
+
+                // 支持解析以后直接通过浏览器来打开
+                Desktop desktop = Desktop.getDesktop();
+                URI uri = new URI(out);
+                desktop.browse(uri);
+
                 endLinePrint();
                 enterAccessCode();
             }
